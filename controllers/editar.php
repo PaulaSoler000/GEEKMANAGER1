03d5b5dd35e0a6c189dcf9453a1e6e6157492fb1
PaulaSoler000 <93978384+PaulaSoler000@users.noreply.github.com>
2023-05-05 08:56:58 +0200

    foto

--- controllers/editar.php
+++ controllers/editar.php
@@ -23,15 +23,17 @@ $imagen = $datos->foto;
     $tipo = $file["type"];
     $ruta_provisional = $file["tmp_name"];
     $size = $file["size"];
-    $dimensiones = getimagesize($ruta_provisional);
-    $witdh = $dimensiones[0];
-    $height = $dimensiones[1];
+
+  
     $carpeta = realpath("../fotos") . "/";
     if ($tipo != 'image/jpg' && $tipo != 'image/JPG' && $tipo != 'image/jpeg' && $tipo != 'image/png' && $tipo != 'image/PNG') {
       echo "Error, el archivo no es una imagen";
     } else if ($size > 3 * 1024 * 1024) {
       echo "Error, el tamaño máximo permitido es un 3MB";
     } else {
+      $dimensiones = getimagesize($ruta_provisional);
+      $witdh = $dimensiones[0];
+      $height = $dimensiones[1];
       $src = $carpeta . $nombre;
 
       move_uploaded_file($ruta_provisional, $src);
