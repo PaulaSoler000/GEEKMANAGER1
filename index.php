ee4c10d79878798ebdf1bc4c6066e5de8902fd7a
PaulaSoler000 <paulawei.s249952@cesurformacion.com>
2023-05-21 12:16:59 +0200

    nada nuevo, footer en index

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Favicon -->
-    <link rel="icon" type="image/png" sizes="32x32" href="../img/logo_transparente.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/logo_transparente.png">
     <!-- CSS-->
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/index1.css">
     <!-- iconos fontawesome-->
     <script src="https://kit.fontawesome.com/4a0af06348.js" crossorigin="anonymous"></script>
     <title>Inicio</title>
@@ -18,24 +18,50 @@
 <body class="fondo">
 
 
-<div class="header_index">
-<img src="img/logo_con_nombre.png">
-<a class="button" href="views/login_view.php">Acceder</a>
-</div>
+    <div class="header_index">
+        <img src="img/logo_con_nombre.png">
+        <a class="button" href="views/login_view.php">Acceder</a>
+    </div>
 
 
-<div class="texto_index">
-<h1 class="h1_index">¡Gestiona tu colección friki como todo un geek!</h1>
+    <div class="wrapper">
+        <div class="texto_index">
+            <h1 class="h1_index">¡Gestiona tu colección friki como todo un geek!</h1>
 
-<p>¡Bienvenidos a GeekManager! La página web diseñada para aquellos que quieren llevar un registro 
-    detallado de sus artículos frikis. En nuestra plataforma podrás añadir, editar y eliminar tus 
-    colecciones con facilidad y rapidez. Olvídate de llevar un registro en papel o de perder la cuenta 
-    de tus adquisiciones. Con GeekManager, tendrás toda la información de tus objetos frikis en un solo lugar. 
-    ¡Regístrate ahora y haz que tu colección destaque!</p>
-</div>
-<br>
-<br>
-<a class="button" href="views/signup_view.php">Regístrate gratis</a>
+            <p>¡Bienvenidos a GeekManager! La página web diseñada para aquellos que quieren llevar un registro
+                detallado de sus artículos frikis. En nuestra plataforma podrás añadir, editar y eliminar tus
+                colecciones con facilidad y rapidez. Olvídate de llevar un registro en papel o de perder la cuenta
+                de tus adquisiciones. Con GeekManager, tendrás toda la información de tus objetos frikis en un solo lugar.
+                ¡Regístrate ahora y haz que tu colección destaque!</p>
+        </div>
+        <br>
+        <br>
+        <a class="button" href="views/signup_view.php">Regístrate gratis</a>
+    </div>
+
+    <!--footer-->
+    <footer class="footer">
+        <div class="footer__addr logo">
+            <a href="index.php">
+                <img src="img/logo_con_nombre.png">
+            </a>
+
+        </div>
+
+        <div class="footer__nav">
+
+
+        </div>
+
+
+        <div class="legal">
+            <p>Copyright &copy; 2023 GeekManager. All rights reserved.</p>
+
+            <div class="legal__links">
+                <a class=" legal__links" href="#">Privacy Policy</a>
+            </div>
+        </div>
+    </footer>
 
 </body>
 
